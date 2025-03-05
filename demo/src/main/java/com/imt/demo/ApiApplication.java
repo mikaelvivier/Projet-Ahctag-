e1f89520fcf5555e727f65ebdb764356a1249c2f
miklvv <mikael2735@gmail.com>
2025-03-05 13:39:21 +0100

    fix: authentication with jwt that runs

--- demo/src/main/java/com/imt/demo/ApiApplication.java
+++ demo/src/main/java/com/imt/demo/ApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = "com.imt.demo.dao")
+@EnableMongoRepositories(basePackages = "com.imt.demo.repository")
 public class ApiApplication {
 
 	public static void main(String[] args) {
