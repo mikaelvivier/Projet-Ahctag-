97c4416908f58bb7f82655f26e4f9badf5042810
miklvv <mikael2735@gmail.com>
2025-03-06 09:38:40 +0100

    chore: configs pour docker

--- demo/src/main/java/com/imt/demo/controller/UserController.java
+++ demo/src/main/java/com/imt/demo/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 @RestController
-@RequestMapping("api/v1/user")
+@RequestMapping("auth")
 public class UserController {
     private UserService userService;
     private JwtGeneratorInterface jwtGenerator;
