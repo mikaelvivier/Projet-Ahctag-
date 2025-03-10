a964da016be9ea527ad40980887fbef840616b79
miklvv <mikael2735@gmail.com>
2025-03-10 09:38:07 +0100

    feat : suppression des fichiers

--- demo/src/main/java/com/imt/demo/exception/MyCustomException.java
+++
@@ -1,9 +0,0 @@
-package com.imt.demo.exception;
-
-public class MyCustomException extends RuntimeException {
-
-    public MyCustomException(String message) {
-        super(message);
-    }
-    
-}
